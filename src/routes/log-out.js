d9eafcdeed1fae9bd4f21d8740750c34c83aad76
Danielle <daniellelee73@gmail.com>
2024-10-21 17:26:57 +0100

    modified contents

--- src/routes/log-out.js
+++ src/routes/log-out.js
@@ -1,10 +1,19 @@
-function post(req, res) {
+const { removeSession } = require("../model/session");
+
+function get(req, res) {
   /**
    * [1] Get the session ID from the cookie
    * [2] Remove that session from the DB
    * [3] Remove the session cookie
    * [4] Redirect back home
    */
+
+  const sid = req.signedCookies.sid;
+  removeSession(sid);
+  res.clearCookie('sid');
+  res.redirect('/')
+
+
   res.status(500).send("");
 }
 
